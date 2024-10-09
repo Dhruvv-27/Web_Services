a8253b14dba7b561eb392d8f0edf2050db28118a
chetan <dork.knight04@gmail.com>
2024-10-10 01:10:38 +0400

    comments added

--- finexo-html/travel_expenses.php
+++ finexo-html/travel_expenses.php
@@ -1,4 +1,5 @@
 <?php
+// Set the header to return a JSON response
 header("Content-Type: application/json");
 
 // Load the JSON file containing travel expenses data.
@@ -9,8 +10,10 @@ $country = $_GET["country"];
 
 // Check if the country exists in the data.
 if (isset($data[$country])) {
+    // If the country is found, return its data as a JSON response
     echo json_encode($data[$country]);
 } else {
+    // If the country is not found, return an error message
     echo json_encode(["error" => "Country not found"]);
 }
 ?>
